412ed8e788011ea6c9e19142cb981dbd2cc5d36f
Nikita Zhigalin <30mayday@gmail.com>
2022-06-01 19:24:31 +0300

    update 01.06.2022

--- src/main/java/zhigalin/predictions/model/event/HeadToHead.java
+++ src/main/java/zhigalin/predictions/model/event/HeadToHead.java
@@ -39,37 +39,4 @@ public class HeadToHead {
     private LocalDateTime localDateTime;
 
     private String leagueName;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        HeadToHead that = (HeadToHead) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (homeTeam != null ? !homeTeam.equals(that.homeTeam) : that.homeTeam != null) return false;
-        if (awayTeam != null ? !awayTeam.equals(that.awayTeam) : that.awayTeam != null) return false;
-        if (homeTeamScore != null ? !homeTeamScore.equals(that.homeTeamScore) : that.homeTeamScore != null)
-            return false;
-        if (awayTeamScore != null ? !awayTeamScore.equals(that.awayTeamScore) : that.awayTeamScore != null)
-            return false;
-        if (localDateTime != null ? !localDateTime.equals(that.localDateTime) : that.localDateTime != null)
-            return false;
-        if (leagueName != null ? !leagueName.equals(that.leagueName) : that.leagueName != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (homeTeam != null ? homeTeam.hashCode() : 0);
-        result = 31 * result + (awayTeam != null ? awayTeam.hashCode() : 0);
-        result = 31 * result + (homeTeamScore != null ? homeTeamScore.hashCode() : 0);
-        result = 31 * result + (awayTeamScore != null ? awayTeamScore.hashCode() : 0);
-        result = 31 * result + (localDateTime != null ? localDateTime.hashCode() : 0);
-        result = 31 * result + (leagueName != null ? leagueName.hashCode() : 0);
-        return result;
-    }
 }
