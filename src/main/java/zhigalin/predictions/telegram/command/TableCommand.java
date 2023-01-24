3ba8605d6d7b84348a347857cbb1ae1414571b4c
Nikita Zhigalin <30mayday@gmail.com>
2023-01-24 13:55:30 +0300

    change entity to dto 24.01.2023

--- src/main/java/zhigalin/predictions/telegram/command/TableCommand.java
+++ src/main/java/zhigalin/predictions/telegram/command/TableCommand.java
@@ -2,8 +2,7 @@ package zhigalin.predictions.telegram.command;
 
 import lombok.RequiredArgsConstructor;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import zhigalin.predictions.converter.football.StandingMapper;
-import zhigalin.predictions.model.football.Standing;
+import zhigalin.predictions.dto.football.StandingDto;
 import zhigalin.predictions.service.football.StandingService;
 import zhigalin.predictions.telegram.service.SendBotMessageService;
 
@@ -14,47 +13,46 @@ public class TableCommand implements Command {
 
     private final SendBotMessageService sendBotMessageService;
     private final StandingService standingService;
-    private final StandingMapper standingMapper;
 
     @Override
     public void execute(Update update) {
-        List<Standing> list = standingService.getAll().stream().map(standingMapper::toEntity).toList();
+        List<StandingDto> list = standingService.getAll();
         int i = 1;
         StringBuilder builder = new StringBuilder();
         builder.append("`").append("  ").append("КЛУБ ").append("И  ").append("В  ").append("Н  ").append("П  ")
                 .append("ЗМ ").append("ПМ ").append("О  ").append("`").append("\n");
-        for (Standing standing : list) {
+        for (StandingDto standingDto : list) {
             builder.append("`").append(i++).append(".");
             if (i < 11) {
                 builder.append(" ");
             }
-            builder.append(standing.getTeam().getCode()).append(" ")
-                    .append(standing.getGames()).append(" ");
-            if (standing.getGames() < 10) {
+            builder.append(standingDto.getTeam().getCode()).append(" ")
+                    .append(standingDto.getGames()).append(" ");
+            if (standingDto.getGames() < 10) {
                 builder.append(" ");
             }
-            builder.append(standing.getWon()).append(" ");
-            if (standing.getWon() < 10) {
+            builder.append(standingDto.getWon()).append(" ");
+            if (standingDto.getWon() < 10) {
                 builder.append(" ");
             }
-            builder.append(standing.getDraw()).append(" ");
-            if (standing.getDraw() < 10) {
+            builder.append(standingDto.getDraw()).append(" ");
+            if (standingDto.getDraw() < 10) {
                 builder.append(" ");
             }
-            builder.append(standing.getLost()).append(" ");
-            if (standing.getLost() < 10) {
+            builder.append(standingDto.getLost()).append(" ");
+            if (standingDto.getLost() < 10) {
                 builder.append(" ");
             }
-            builder.append(standing.getGoalsScored()).append(" ");
-            if (standing.getGoalsScored() < 10) {
+            builder.append(standingDto.getGoalsScored()).append(" ");
+            if (standingDto.getGoalsScored() < 10) {
                 builder.append(" ");
             }
-            builder.append(standing.getGoalsAgainst()).append(" ");
-            if (standing.getGoalsAgainst() < 10) {
+            builder.append(standingDto.getGoalsAgainst()).append(" ");
+            if (standingDto.getGoalsAgainst() < 10) {
                 builder.append(" ");
             }
-            builder.append(standing.getPoints()).append(" ");
-            if (standing.getPoints() < 10) {
+            builder.append(standingDto.getPoints()).append(" ");
+            if (standingDto.getPoints() < 10) {
                 builder.append(" ");
             }
             builder.append("`").append("\n");
