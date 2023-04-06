8bc67fa79c495815c3deb9830861ecc02ff6bce5
Nikita Zhigalin <30mayday@gmail.com>
2023-04-06 17:32:26 +0300

    critical error fix 06-04-2023

--- src/main/java/zhigalin/predictions/dto/predict/PredictionDto.java
+++ src/main/java/zhigalin/predictions/dto/predict/PredictionDto.java
@@ -2,15 +2,15 @@ package zhigalin.predictions.dto.predict;
 
 import lombok.Builder;
 import lombok.Value;
-import zhigalin.predictions.dto.user.UserDto;
 import zhigalin.predictions.model.event.Match;
+import zhigalin.predictions.model.user.User;
 
 @Value
 @Builder
 public class PredictionDto {
     Long id;
     Match match;
-    UserDto user;
+    User user;
     Integer homeTeamScore;
     Integer awayTeamScore;
     Long points;
